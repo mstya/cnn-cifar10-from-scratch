d9adc50dd7cb500495f62c5de585cd7e7efd2dfd
vyankov <slavikbr7@gmail.com>
2026-08-04 11:16:45 +0300

    Model arc update

--- src/model.py
+++ src/model.py
@@ -2,14 +2,14 @@ from torch import nn
 
 
 class CNNBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1):
+    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1, max_pool_s=2):
         super(CNNBlock, self).__init__()
 
         self.block = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size, padding=padding),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(),
-            nn.MaxPool2d(2, stride=2)
+            nn.MaxPool2d(2, stride=max_pool_s)
         )
 
     def forward(self, x):
@@ -33,6 +33,33 @@ class SimpleCNN(nn.Module):
     def forward(self, x):
         x = self.conv_block1(x)
         x = self.conv_block2(x)
+
+        x = self.classifier(x)
+
+        return x
+
+class SimpleCNNConv3(nn.Module):
+    def __init__(self, num_classes, dropout):
+        super(SimpleCNNConv3, self).__init__()
+
+        self.conv_block1 = CNNBlock(3, 32)
+        self.conv_block2 = CNNBlock(32, 64)
+        self.conv_block3 = CNNBlock(64, 128, max_pool_s=1)
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(6272, 512),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = self.conv_block3(x)
 
         x = self.classifier(x)
 
